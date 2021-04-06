6c7dcbee9b3243b85c421877133cf75bdca327b5
AlexJ <linctjabc@gmail.com>
2021-04-06 11:36:03 -0400

    added register module, bug with react hook not update correctly

--- foodiebudgettools/src/index.js
+++ foodiebudgettools/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App_BM from '../src/components/HomePage_BM/App_BM';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App_BM />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
