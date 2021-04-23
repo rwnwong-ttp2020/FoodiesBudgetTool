b5e615ce97582a2d6b36a99c1e393a9e0e8eeaa3
rwnwong <rwnwong@gmail.com>
2021-04-22 20:37:49 -0400

    resolved conflict with branch Ibrahima

--- foodiebudgettools/src/App.js
+++ foodiebudgettools/src/App.js
@@ -1,14 +1,11 @@
 import LoginModule from "../src/components/LoginModule_JL/LoginModule";
-<<<<<<< HEAD
 
 //import MapModule from "../src/components/Map_JL/MapModule";
 import APP_BM from "../src/components/HomePage_BM/App_BM";
 import HeroSection from "./components/HomePage_BM/HeroSection";
-=======
 import MapModule from "../src/components/Map_JL/MapModule";
 import RegisterModule from "../src/components/LoginModule_JL/RegisterModule";
 import Searchbar from "../src/components/SearchModule/Searchbar";
->>>>>>> main
 function App() {
   return (
     <div>
