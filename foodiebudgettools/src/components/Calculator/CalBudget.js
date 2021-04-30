ce9f898674aec04e35f49cb18202ebe7a4dc439c
https://github.com/Ibsoncouldy/CSC-380.git <78333703+Ibsoncouldy@users.noreply.github.com>
2021-04-30 00:00:04 -0400

    Fixed the Budget calculation

--- foodiebudgettools/src/components/Calculator/CalBudget.js
+++ foodiebudgettools/src/components/Calculator/CalBudget.js
@@ -7,7 +7,7 @@ import plusPic from "./icon/plus.png"
 
 function CalBudget() {
     const [tabNumber, setTabNumber] = useState(0)
-
+    const [balance, setBalance] = useState(0)
     const [formData, setFormData] = useState({
         restaurantName: "",
         expense: "",
@@ -16,6 +16,7 @@ function CalBudget() {
     })
 
     const [restaurantExpenses, setRestaurantExpenses] = useState([])
+    const [incomes, setIncomes] = useState([])
 
     const onChange = (e) => {
         e.preventDefault()
@@ -27,12 +28,27 @@ function CalBudget() {
 
     const onAddRestaurantExpense = () => {
         let expenses = [...restaurantExpenses]
+        let exp = +formData.expense //turns to a number
+        setBalance(balance - exp)
         expenses.push({ restaurant: formData.restaurantName, expense: formData.expense })
         setRestaurantExpenses(expenses)
         setFormData({
             ...formData,
             restaurantName: "",
             expense: ""
+        })
+    }
+
+    const onAddIncome = () => {
+        let incomeArray = [...incomes]
+        let inc = +formData.incomeAmount
+        setBalance(balance + inc)
+        incomeArray.push({ name: formData.incomeName, amount: formData.incomeAmount })
+        setIncomes(incomeArray)
+        setFormData({
+            ...formData,
+            incomeAmount: "",
+            incomeName: ""
         })
     }
 
@@ -47,8 +63,20 @@ function CalBudget() {
                         Balance
                 </div>
                     <div className="value">
-                        <small>$</small>0
-                </div>
+                        {
+                            balance < 0 ? (
+                                <>
+                                    <small>-$</small>{Math.abs(balance)}
+                                </>
+                            ) : (
+                                    <>
+                                        <small>$</small>{balance}
+                                    </>
+                                )
+                        }
+
+
+                    </div>
                 </div>
                 <div className="account">
                     <div className="income">
@@ -80,11 +108,13 @@ function CalBudget() {
                 {
                     tabNumber === 0 ?
                         (
-                            <div id="income">
+                            <div id="expense">
                                 <ul className="list">
                                     {
                                         restaurantExpenses.map((res, index) => (
-                                            <li key={index}>{res.restaurant} {res.expense}</li>
+                                            <li key={index} className="expense">
+                                                <div className="entry">{res.restaurant}: ${res.expense}</div>
+                                            </li>
                                         ))
                                     }
                                 </ul>
@@ -109,8 +139,16 @@ function CalBudget() {
                             </div>
                         ) : tabNumber === 1 ?
                             (
-                                <div id="expense">
-                                    <ul className="list"></ul>
+                                <div id="income">
+                                    <ul className="list">
+                                        {
+                                            incomes.map((income, index) => (
+                                                <li key={index}>
+                                                    <div className="entry">{income.name}: ${income.amount}</div>
+                                                </li>
+                                            ))
+                                        }
+                                    </ul>
                                     <div className="input">
                                         <input
                                             type="text"
@@ -126,7 +164,7 @@ function CalBudget() {
                                             value={formData.incomeAmount}
                                             placeholder="$0"
                                             onChange={onChange} />
-                                        <div className="add-expense" onClick={onAddRestaurantExpense}>
+                                        <div className="add-expense" onClick={onAddIncome}>
                                             <img src={plusPic} alt="" />
                                         </div>
                                     </div>
@@ -135,8 +173,17 @@ function CalBudget() {
                                 <div id="all">
                                     <ul className="list">
                                         {
+                                            incomes.map((income, index) => (
+                                                <li key={index}>
+                                                    <div className="entry">{income.name}: ${income.amount}</div>
+                                                </li>
+                                            ))
+                                        }
+                                        {
                                             restaurantExpenses.map((res, index) => (
-                                                <li key={index}>{res.restaurant} {res.expense}</li>
+                                                <li key={index} className="expense">
+                                                    <div className="entry">{res.restaurant}: ${res.expense}</div>
+                                                </li>
                                             ))
                                         }
                                     </ul>
