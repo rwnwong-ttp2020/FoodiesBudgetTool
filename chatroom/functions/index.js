172ab43c41f3873ceb681679f693ba4e7818828d
Tangbodi <78437899+Tangbodi@users.noreply.github.com>
2021-05-03 18:46:31 -0400

    May3rd

--- chatroom/functions/index.js
+++ chatroom/functions/index.js
@@ -1,11 +1,19 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
 const Filter = require('bad-words');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
 const db = admin.firestore();
 
-exports.detectEvilUsers = functions.firestore
+exports.BadUsers = functions.firestore
        .document('messages/{msgId}')
        .onCreate(async (doc, ctx) => {
 
