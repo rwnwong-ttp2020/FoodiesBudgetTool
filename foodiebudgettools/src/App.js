652581a3a1d0653cde79bb180e81400343a3993f
rwnwong <rwnwong@gmail.com>
2021-05-04 22:03:04 -0400

    minor changes to user story 1 - don't push

--- foodiebudgettools/src/App.js
+++ foodiebudgettools/src/App.js
@@ -6,10 +6,11 @@ import HeroSection from "./components/HomePage_BM/HeroSection";
 import MapModule from "../src/components/Map_JL/MapModule";
 import RegisterModule from "../src/components/LoginModule_JL/RegisterModule";
 import Searchbar from "../src/components/SearchModule/Searchbar";
+import BudgetInterface from "../src/components/UserStory1/budgetInterface";
 function App() {
   return (
     <div>
-      <LoginModule/>
+      <BudgetInterface/>
     </div>
   );
 }
