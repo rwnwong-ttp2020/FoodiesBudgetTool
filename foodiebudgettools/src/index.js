2b51300ac612b04d59bcd9e38f31643b5e0ef9d2
AlexJ <linctjabc@gmail.com>
2021-05-21 19:21:36 -0400

    make change

--- foodiebudgettools/src/index.js
+++ foodiebudgettools/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-//import App from "../src/components/client/src/component/App";
-
 
 ReactDOM.render(
     <App />,
